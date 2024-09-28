57350dd69280c5c6d8fdd6c69807841210774bdf
Prabhakar Pal <prabhakar.pal00@gmail.com>
2024-09-28 22:04:01 +0530

    tool outline

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@
 #include "printers/function_printer.h"
 #include "utils/utils.h"
 
+#include "clang/Tooling/Refactoring/AtomicChange.h"
+#include "clang/Tooling/Transformer/RewriteRule.h"
+#include "clang/Tooling/Transformer/Stencil.h"
+#include "clang/Tooling/Transformer/Transformer.h"
+
 #include <clang/AST/DeclBase.h>
 
+using namespace clang;
 using namespace clang::tooling;
-using namespace llvm;
 using namespace clang::ast_matchers;
+using namespace clang::transformer;
 
 int main(int argc, const char **argv) {
   llvm::cl::OptionCategory ctCategory("clang-tool options");
@@ -37,13 +43,44 @@ int main(int argc, const char **argv) {
         utils::getCompileArgs(compileCommands);
     compileArgs.push_back("-I" + utils::getClangBuiltInIncludePath(argv[0]));
 
-    printers::FunctionPrinter Printer;
-    auto matcher = matchers::get_decl_matchers(sourceFile);
-    MatchFinder Finder;
-    Finder.addMatcher(matcher, &Printer);
+    // auto matcher = matchers::get_decl_matchers(sourceFile);
+    auto matcher = functionDecl(isDefinition()).bind("fn");
+    auto action = changeTo(name("fn"), cat(name("fn"), "_renamed"));
+    // auto rewrite_rule = makeRule(matcher, action);
+    AtomicChanges Changes;
+    auto Consumer = [&](Expected<MutableArrayRef<AtomicChange>> C) {
+      if (C) {
+        llvm::outs() << "Matches\n";
+        Changes.insert(Changes.end(), std::make_move_iterator(C->begin()),
+                       std::make_move_iterator(C->end()));
+      } else {
+        llvm::errs() << "Error generating changes: "
+                     << llvm::toString(C.takeError()) << "\n";
+      }
+    };
 
-    utils::customRunToolOnCodeWithArgs(newFrontendActionFactory(&Finder)->create(),
-                                       sourcetxt, compileArgs, sourceFile);
+    // Transformer transformer(makeRule(functionDecl(hasName("bad")).bind("f"),
+    //                                  changeTo(name("f"), cat("good"))),
+    //                         std::move(Consumer));
+    Transformer transformer(makeRule(matcher, action), std::move(Consumer));
+    MatchFinder finder;
+    transformer.registerMatchers(&finder);
+
+    utils::customRunToolOnCodeWithArgs(
+        newFrontendActionFactory(&finder)->create(), sourcetxt, compileArgs,
+        sourceFile);
+
+    auto changed_code =
+        applyAtomicChanges(sourceFile, sourcetxt, Changes, ApplyChangesSpec());
+
+    // llvm::outs() << *changed_code << "\n";
+    std::error_code EC;
+    llvm::raw_fd_ostream OS(sourceFile, EC, llvm::sys::fs::OF_TextWithCRLF);
+    if (EC) {
+      llvm::errs() << EC.message() << "\n";
+      break;
+    }
+    OS << *changed_code;
   }
 
   return 0;
--- tests/fn.cpp
+++ tests/fn.cpp
@@ -1,6 +1,3 @@
-#include "./fn.h"
-#include <iostream>
-
 class Toy {
   void do_something(int y) {
     x = y;
@@ -15,12 +12,9 @@ void foo(int a, int b) { int x = a + b; }
 
 int bar(int x) { return x * 2; }
 
+int bad(int y) { return y+4; }
+
 int main() {
   auto b = bar(2);
-  auto o = testing::Other(4);
-  o.foo(0.0f);
-  o.foo(6);
-  std::cout << o.get_x() << std::endl;
-
   return 0;
 }
